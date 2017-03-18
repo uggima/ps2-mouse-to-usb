63268839867663a1905be4d42d7b5572d65aa383
uggima <lukedw@hotmail.com>
2017-03-18 05:12:37 +0000

    Update ps2_mouse.ino

--- ps2_mouse.ino
+++ ps2_mouse.ino
@@ -21,7 +21,10 @@ void loop() {
   // If not using plentiful DigiMouse.delay(), make sure to call
   //  DigiMouse.update(); //at least every 50ms
 
-  if (!i)//initial state, just send blank data packets till mouse has properly woken up (mine takes 600-750ms) at which point it will say its performed its POST and all is well
+        //initial state, just send blank data packets till mouse has properly woken up 
+        //(mine takes 600-750ms) at which point it will say its performed its POST 
+        //and all is well (which is compleltely ignored)
+  if (!i)
     DigiMouse.update();
     
   if (i == 0)//if initial state and 3s have passed
@@ -34,17 +37,24 @@ void loop() {
       mouse_init();
     }
   }
-
-  if (i && usbTxStatus1.len & 0x10)//unsure where i grabbed this from, but checks to see if the TX system is inactive which with a reduction of the delays on setting ps2 pins to the master TX mode make for a nice smooth action
-                                    //before shorter ps2 tx mode delays were used ps2 communication was only ever ~4th-12th usb packet, more investigation might be good.
+  /*
+    unsure where i grabbed the & 0x10 from, but checks to see if the TX system is 
+    inactive which with a reduction of the delays on setting ps2 pins to master TX
+    mode make for a nice smooth action (one ps2 packet per usb)
+    
+    before shorter ps2 tx mode delays were used ps2 communication was only every
+    ~4th-12th usb packet, more investigation might be good??
+  */
+  
+  if (i && usbTxStatus1.len & 0x10)                                    
   {
     mouse.write(0xeb);  // give me data!
     mouse.read();      // ignore ack
     mstat = mouse.read();
     mx = mouse.read();
     my = mouse.read();
-    DigiMouse.move(mx, my*-1,0,mstat);//y var is inverted on my touchpad for some reason, and "4th" bit always on?? (pad only has 2 buttons)
-    DigiMouse.update();
+    DigiMouse.move(mx, my*-1,0,mstat);//y var is inverted on my touchpad for some reason, 
+    DigiMouse.update();               //and "4th" button bit always on?? (pad only has 2 buttons)
   }
 }
 
@@ -57,5 +67,7 @@ void mouse_init()
   mouse.read();  // blank */
   mouse.write(0xf0);  // remote mode
   mouse.read();  // ack
-  DigiMouse.delay(25);              //this does annoy usb bus, but its only once and previous calls are ~1ms each, single byte transfered. So roughtly 20ms less than a complete usb timeout.
+  DigiMouse.delay(25); //this does annoy the usb controller, but its only once 
+                       //and previous calls are ~1ms each (a single byte transfered). 
+                       //So roughtly 20ms less than a complete usb timeout?
 }
